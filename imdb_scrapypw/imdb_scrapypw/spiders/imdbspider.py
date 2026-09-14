5e7ac889b277389451952e35fd9191d5becfce31
asnganje <abd.nganje@gmail.com>
2026-09-14 06:47:35 +0300

    add well thought out logic for handling movie selection based on specific categories

--- imdb_scrapypw/imdb_scrapypw/spiders/imdbspider.py
+++ imdb_scrapypw/imdb_scrapypw/spiders/imdbspider.py
@@ -40,6 +40,26 @@ class ImdbspiderSpider(scrapy.Spider):
         try:
             button = page.locator("button#suggestion-search-button")
             await button.click()
-            sleep(10)
+            await page.get_by_text("Movies, TV & more").click()
+            title_accordion = page.get_by_test_id("accordion-item-titleTypeAccordion")
+            if title_accordion.get_attribute("aria-label") == "Expand Title type":
+                await title_accordion.click()
+            movie_c = page.get_by_test_id("test-chip-id-movie")
+            await movie_c.click()
+
+            genre_accordion = page.get_by_test_id("accordion-item-genreAccordion")
+            if await genre_accordion.get_attribute("aria-label") == "Expand Genre":
+                await genre_accordion.click()
+            comedy_c = page.get_by_test_id("test-chip-id-Comedy")
+            await comedy_c.click()
+
+            awards_accordion = page.get_by_test_id("accordion-item-awardsAccordion")
+            if await awards_accordion.get_attribute("aria-label") == "Expand Awards & recognition":
+                await awards_accordion.click()
+            awards_b = page.get_by_test_id("test-chip-id-oscar-nominated")
+            await awards_b.click()
+            await page.get_by_test_id("adv-search-get-results").click()
+
+            sleep(5)
         finally:
             await page.close()
